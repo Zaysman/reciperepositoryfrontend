003c4b3ba2fa6c2e5c37c8ffe4c21065b923b740
Isaiah <i.x.smith137@gmail.com>
2024-09-30 17:26:11 -0400

    Updated ViewRecipes component to use useEffect hooks to retrieve recipe information from the backend

--- src/components/ViewRecipes/ViewRecipes.js
+++ src/components/ViewRecipes/ViewRecipes.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './ViewRecipes.css'
 import Recipe from '../../objects/Recipe';
 import User from '../../objects/User'
+import JSONRequests from 'libraries/JSONRequests';
 
-
+//Environment variables
 const backendUrl = process.env.REACT_APP_RECIPE_REPOSITORY_BACKEND_URL;
 const getRecipesByAuthorIDUrl = process.env.REACT_APP_RECIPE_REPOSITORY_GET_RECIPES_BY_AUTHOR_ID;
 const getFavoritedRecipesByUserIDUrl = process.env.REACT_APP_RECIPE_REPOSITORY_GET_FAVORITEDRECIPES_BY_USER_ID;
@@ -15,20 +16,116 @@ function ViewRecipes() {
     const location = useLocation();
 
     const user = location.state?.user;
-    const [recipe, setRecipe] = useState(null); //The recipe that the user will select to view
+    const [recipe, setRecipe] = useState(null); //The recipe type that the user will select to view
     
-    function handleSetRecipe(selectedRecipe) {
-        setRecipe(selectedRecipe);
-        console.log("selectedRecipe", recipe);
-    }
+    const [recipes, setRecipes] = useState([]); //State to store the recipes retrieve from the backend
+    const [selectedRecipeType, setSelectedRecipeType] = useState(''); //State to track the selected radio button
+
+    const jsonRequests = new JSONRequests();
+    const isFirstRender = useRef(true); //Ref to track if it's the 1st render
 
     console.log("User", user);
+    const userIDString = user.userID.toString();
 
-    //State to store the recipes retrieve from the backend
-    const [recipes, setRecipes] = useState([]);
+    
 
-    //State to track the selected radio button
-    const [selectedRecipeType, setSelectedRecipeType] = useState('');
+    //Urls
+    const getAddedRecipesUrl = backendUrl + getRecipesByAuthorIDUrl + userIDString;
+    const getFavoritedRecipesUrl = backendUrl + getFavoritedRecipesByUserIDUrl + userIDString;
+
+    //useEffect hooks go here.
+
+    //useEffect to handle when Added Recipes is added.
+    useEffect(() => {
+        const getAddedRecipes = async () => {
+
+            if(isFirstRender.current) {
+                //Skip the first render
+                isFirstRender.current = false;
+                console.log("selectedRecipeType on page load up: ", selectedRecipeType);
+                return; //Halt execution of effect on initial render of the page.
+            }
+
+            //If the selectedRecipeType isn't 'added', have this effect return
+            if(selectedRecipeType != 'added') {
+                console.log("selectedRecipeType:", selectedRecipeType); //Log the recipe type.
+                return; //Halt execution if the selectedRecipeType isn't added.
+            }
+
+            //confirm destination
+            console.log("getAddedRecipesUrl", getAddedRecipesUrl);
+
+            //send the fetch request and get the data
+            const data = await jsonRequests.sendGetRequest(getAddedRecipesUrl);
+
+            //Handle the data returned from the fetch request
+            console.log("data retrieved from backend", data); //Log data from response
+
+            //update the recipes state with the retrieved recipes
+            setRecipes(data);
+        };
+        getAddedRecipes();
+    }, [selectedRecipeType]);
+
+
+    //useEffect to handle when Favorited Recipes is added.
+    useEffect(() => {
+        const getFavoriteRecipes = async () => {
+
+            if(isFirstRender.current) {
+                //Skip the first render
+                isFirstRender.current = false;
+                console.log("selectedRecipeType on page load up: ", selectedRecipeType);
+                return; //Halt execution of effect on initial render of the page.
+            }
+
+            //If the selectedRecipeType isn't 'favorited', have the effect cease execution
+            if(selectedRecipeType != 'favorited') {
+                console.log("selectedRecipeType:", selectedRecipeType); //Log the recipe type.
+                return; //Halt execution if the selectedRecipeType isn't added.
+            }
+
+            //confirm destination
+            console.log("getFavoritedRecipes", getFavoritedRecipesUrl);
+
+            //send the fetch request and get the data
+            const data = await jsonRequests.sendGetRequest(getFavoritedRecipesUrl);
+
+            //Handle the data returned from the fetch request
+            console.log("data retrieved from backend", data);
+
+            var favoritedRecipeIDs = []; //Iterate through favoritedRecipeData and compose a list of the recipeIDs we need
+
+            for(let i = 0; i < data.length; i++) {
+                favoritedRecipeIDs.push(data[i].favoritedRecipeID);
+            }
+
+            //Confirm favoritedRecipeIDs has the correct data.
+            console.log("favorited Recipe IDs", favoritedRecipeIDs);
+
+            try {
+                console.log("Sending Get Request to get favorite recipes by recipeIDs");
+
+                //confirm destination
+                const params = favoritedRecipeIDs.map(id => `favoritedRecipeIDs=${id}`).join('&');
+                const getRecipesByRecipeIDsDestination = `${backendUrl}${getRecipesByRecipeIDsUrl}?${params}`;
+                console.log("get recipes using multiple recipeIDs url", getRecipesByRecipeIDsDestination);
+
+                const favoriteRecipeData = await jsonRequests.sendGetRequest(getRecipesByRecipeIDsDestination);
+                setRecipes(favoriteRecipeData);
+
+
+            } catch(error) {
+                console.error("There was an issue retrieving the favorited recipes", error);
+            }
+
+            //update the recipes state with the retrieved recipes
+            //setRecipes(data);
+        };
+        getFavoriteRecipes();
+    }, [selectedRecipeType]) //Use this effect when the selected Recipe Type changes
+
+
 
     function toHome() {
         navigate("/home");
@@ -40,150 +137,13 @@ function ViewRecipes() {
     }
 
     const handleAddedRecipesRadioInput = async (event) => {
-        //Set the selected radio button. Make sure to put this first so the UI reflects changes without any delay.
         setSelectedRecipeType('added');
-        
         console.log("Retrieving Added recipes by user");
-
-        try {
-            console.log("Sending Get Request for Added Recipes to Backend");
-            const userIDString = user.userID.toString();
-            
-            //confirm the destination.
-            const getAddedRecipesUrl = backendUrl + getRecipesByAuthorIDUrl + userIDString;
-            console.log("getAddedRecipesUrl", getAddedRecipesUrl);
-
-            //send the fetch request
-            fetch(getAddedRecipesUrl).then(response => {
-                //If we don't receive an ok response (200-299), print the error message
-                if(!response.ok) {
-                    throw new Error("Network response was not ok " + response.statusText);
-                }
-
-                //Handle retrieving the response. Catch error if one occurs
-                return response.json().catch(err => {
-                    console.error("Failed to parse JSON:", err);
-
-                    throw new Error("Invalid JSON response");
-                });
-
-            //Handle the data returned from the fetch request
-            }).then(data => {
-                console.log("data retrieved from backend", data); //Log data from response
-
-                //update the state with the retrieved recipes
-                setRecipes(data);
-            })
-
-        } catch(error) {
-            console.error("there was an issue retrieving the recipes added by user:", error);
-        }
-    };
+    }
 
     const handleFavoritedRecipesRadioInput = async (event) => {
-         //Set the selected radio button state. Do this first so that the UI updates without delay.
-         setSelectedRecipeType('favorited');
-        
-         console.log("Retrieving Favorited Recipes by user");
-
-        var favoritedRecipesData; //Data from the favoritedRecipes Table will be held here.
-
-        //get FavoritedRecipe Table
-         try {
-            console.log("Sending Get Request to get favorited recipes by userID")
-            const userIDString = user.userID.toString();
-
-            //confirm the destination
-            const getFavoritedRecipesUrl = backendUrl + getFavoritedRecipesByUserIDUrl + userIDString;
-            console.log("getFavoritedRecipes Table data", getFavoritedRecipesUrl);
-
-            //send the fetch request
-            await fetch(getFavoritedRecipesUrl).then(response => {
-                //If we don't receive an ok response (200-299), print the error message.
-                if(!response.ok) {
-                    throw new Error("Network response was not ok " + response.statusText);
-                }
-
-                //Handle retrieving the response. Catch error if one occurs
-                return response.json().catch(err => {
-                    console.error("Failed to parse JSON:", err);
-
-                    throw new Error("Invalid JSON response");
-
-                });
-
-            //Handle the data returned from the fetch request
-            }).then(data => {
-                console.log("data", data)
-
-                favoritedRecipesData = data;
-            })
-
-        } catch(error) {
-            console.error(error);
-        }
-
-        //Confirm we have the correct data from the favoritedRecipes Table
-        console.log("favoritedRecipesData", favoritedRecipesData);
-
-
-        //Iterate through favoritedRecipeData and compose a list of the recipeIDs we need
-        var favoritedRecipeIDs = [];
-        
-        for(let i = 0; i < favoritedRecipesData.length; i++) {
-            console.log("id to add", favoritedRecipesData[i].favoritedRecipeID)
-            favoritedRecipeIDs.push(favoritedRecipesData[i].favoritedRecipeID);
-        }
-
-        //Confirm favoritedRecipeIDs has the correct data.
-        console.log("favorited Recipe IDs", favoritedRecipeIDs);
-        
-        //Send the list to retrieve the recipes.
-        try {
-            console.log("Sending Get Request to get recipes by recipeIDs")
-
-            //confirm destination
-            const params = favoritedRecipeIDs.map(id => `favoritedRecipeIDs=${id}`).join('&');
-            const getRecipesByRecipeIDsDestination = `${backendUrl}${getRecipesByRecipeIDsUrl}?${params}`;
-            console.log("get recipes using multiple recipeIDs url", getRecipesByRecipeIDsDestination);
-
-
-            
-            //send the fetch request
-            await fetch(getRecipesByRecipeIDsDestination, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }).then(response => {
-                //If we don't receive an ok response (200-299), print the error message.
-                if(!response.ok) {
-                    throw new Error("Network response was not ok " + response.statusText);
-                }
-
-                //Handle retrieving the response. Catch error if one occurs
-                return response.json().catch(err => {
-                    console.error("Failed to parse JSON:", err);
-
-                    throw new Error("Invalid JSON response");
-
-                });
-            //Handle the data returned from the fetch request
-            }).then(data => {
-                //print data
-                console.log("data retrieved from backend");
-
-
-                //update the recipes state with the retrieved recipes
-                setRecipes(data);
-            })
-
-
-        } catch(error) {
-            console.error("There was an issue retrieving the favorited recipes", error);
-        }
-
-        //setRecipes([]);
+        setSelectedRecipeType('favorited');
+        console.log("Retrieving Added recipes by user");
     }
 
     return(
@@ -191,9 +151,11 @@ function ViewRecipes() {
             <h2>View Recipes</h2>
             <div className="radioBtn-container">
                 {/*Bind the checked state of each radio button to the selectedRecipeType state. Update selectedRecipeType when a radio button is clicked.*/}
-                <input type="radio" id="added" name="recipeType" checked={selectedRecipeType === 'added'} onChange={handleAddedRecipesRadioInput}  value="added"/>
+                {<input type="radio" id="added" name="recipeType" checked={selectedRecipeType === 'added'} onChange={handleAddedRecipesRadioInput}  value="added"/>
+                }
                 <label htmlFor="added">Added Recipes</label>
-                <input type="radio" id="favorited" name="recipeType" checked={selectedRecipeType === 'favorited'} onChange={handleFavoritedRecipesRadioInput} value="favorite"/>
+                {<input type="radio" id="favorited" name="recipeType" checked={selectedRecipeType === 'favorited'} onChange={handleFavoritedRecipesRadioInput} value="favorite"/>
+                }
                 <label htmlFor="favorited">Favorited Recipes</label>
             </div>
 
